23b768bbb8c63dd1e002162eab5a6256758a8812
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-12-15 17:40:54 -0700

    added inquirer, all functions calling and responding

--- liri.js
+++ liri.js
@@ -7,8 +7,8 @@ moment().format();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify({
-        id: keys.spotify.id,
-        secret: keys.spotify.secret
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
 });
 
 
@@ -17,164 +17,197 @@ var spotify = new Spotify({
 var userRequest = process.argv.slice(2).join('+');
 var divider = "-------------------------------";
 
-function makeRequest() {
-        inquirer
-                .prompt([{
-                        type: "list",
-                        message: "What would you like to search?",
-                        name: "userInput",
-                        choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"]
+function makeChoice() {
+    inquirer
+        .prompt([{
+            type: "list",
+            message: "What would you like to search?",
+            name: "userInput",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"]
+        }
+        ]).then(function (media) {
+            function selection() {
+                if (media.userInput === "concert-this") {
+                    concerts();
                 }
-                ]).then(function (media) {
-                        if (media.choices === "concert-this") {
-                                return concerts(media.userInput, function (error, data) {
-                                        if (error) {
-                                                return console.log(error);
-                                        }
-                                });
-                        }
-                        if (media.choices === "spotify-this-song") {
-                                return songs();
-                        }
-                        if (media.choices === "movie-this") {
-                                return movies();
-                        }
-                        if (media.choices === "do-what-it-says") {
-                                return other();
-                        }
-                });
+                else if (media.userInput === "spotify-this-song") {
+                    songs();
+                }
+                else if (media.userInput === "movie-this") {
+                    movies();
+                }
+                else if (media.userInput === "do-what-it-says") {
+                    other();
+                }
+            }
+            selection();
+        });
 }
-makeRequest();
+
+makeChoice();
 
 function concerts() {
-        inquirer.prompt([
-                {
-                        type: "input",
-                        message: "Enter the artist's name, song title, or movie title.",
-                        name: "request"
-                }
-        ])
-                .then(function (media) {
-                        console.log(media.request);
-                        if (!media.request) {
-                                media.request = "Coldplay";
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the artist or band name, please.",
+            name: "request"
+        }
+    ]).then(function (media) {
+        var bandName = media.request.replace(/\"/g, '');
+        if (!bandName) {
+            bandName = "Coldplay";
+        }
+        axios.get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=" + keys.bandsintown.bandsAPI).then(
+            function (response) {
+                if (response.data.length === 0 || typeof response.data !== 'object') {
+                    console.log("No information available, maybe not on tour at this time.");
+                    makeChoice();
+                } else {
+                    try {
+                        var concertData = response.data;
+                        for (var i = 0; i < concertData.length; i++) {
+                            console.log(divider);
+                            console.log(`Venue: ${concertData[i].venue.name}`);
+                            console.log(`Location: ${concertData[i].venue.city} ${concertData[i].venue.region}`);
+                            var date = concertData[i].datetime;
+                            date = moment(date).format("MM/DD/YYYY");
+                            console.log(`Date: ${date}`);
+                            console.log(divider);
                         }
-                        axios.get("https://rest.bandsintown.com/artists/" + media.request + "/events?app_id=" + keys.bandsintown.bandsAPI).then(
-                                function (response, error) {
-                                        if (error) {
-                                                return console.log(error);
-                                        } else if (response.data.length === 0) {
-                                                console.log("No information available, maybe not on tour at this time.");
-                                                return; //makeRequest();
-                                        } else {
-                                                var concertData = response.data;
-                                                for (var i = 0; i < concertData.length; i++) {
-                                                        console.log(divider);
-                                                        console.log(`Venue: ${concertData[i].venue.name}`);
-                                                        console.log(`Location: ${concertData[i].venue.city} ${concertData[i].venue.region}`);
-                                                        var date = concertData[i].datetime;
-                                                        date = moment(date).format("MM/DD/YYYY");
-                                                        console.log(`Date: ${date}`);
-                                                        console.log(divider);
-                                                }
-                                        }
-                                }
-                        )
-                });
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+            }, function (error) {
+                console.log("test error: ");
+                if (error) {
+                    console.log(error);
+                }
+            }
+        )
+    });
 }
 
 
-// function songs() {
-//         if (command === 'spotify-this-song') {
-//                 if (!userRequest) {
-//                         userRequest = "The Sign, Ace of Base";
-//                 }
-//                 spotify.search({ type: 'track', query: userRequest }, function (err, data) {
-//                         if (err) {
-//                                 return console.log('Error occurred: ' + err);
-//                         }
-//                         var albumData = data.tracks.items;
-//                         for (var i = 0; i < albumData.length; i++) {
-//                                 var albumInfo = albumData[i].album;
-//                                 var artistName = albumInfo.artists;
-//                                 var songName = albumData[i].name;
-//                                 var songPreview = albumData[i].preview_url;
-//                                 var albumName = albumData[i].album.name;
+function songs() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the song title, please.",
+            name: "request"
+        }
+    ]).then(function (media) {
+        var musicName = media.request.replace(/\"/g, '');
+        if (!musicName) {
+            musicName = "The Sign, Ace of Base";
+        }
+        spotify.search({ type: 'track', query: musicName })
+            .then(function (response) {
+                try {
+                    var albumData = response.tracks.items;
+                    for (var i = 0; i < albumData.length; i++) {
+                        var albumInfo = albumData[i].album;
+                        var artistName = albumInfo.artists;
+                        var songName = albumData[i].name;
+                        var songPreview = albumData[i].preview_url;
+                        var albumName = albumData[i].album.name;
 
-//                                 for (var j = 0; j < artistName.length; j++) {
-//                                         console.log(`Artist(s): ${artistName[j].name}`); //artists
-//                                         console.log(`Name of song: ${songName}`); //name of album
-//                                         console.log(`Preview link from Spotify: ${songPreview}`);
-//                                         console.log(`Album: ${albumName}}`);
-//                                         console.log(divider)
-//                                 };
-//                         };
-//                 });
-//         }
-// };
-// function movies() {
-//         if (command === 'movie-this') {
-//                 if (!userRequest) {
-//                         userRequest = "Mr. Nobody";
-//                 }
-//                 axios.get("http://www.omdbapi.com/?t=" + userRequest + "&y=&plot=short&apikey=" + keys.omdb.moviesAPI).then(
+                        for (var j = 0; j < artistName.length; j++) {
+                            console.log(`Artist(s): ${artistName[j].name}`);
+                            console.log(`Name of song: ${songName}`);
+                            console.log(`Preview link from Spotify: ${songPreview}`);
+                            console.log(`Album: ${albumName}}`);
+                            console.log(divider)
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }, function (error) {
+                console.log("Error: ");
+                if (error) {
+                    console.log(error);
+                }
+            }
+            )
+    });
+}
 
-//                         function (response, error) {
+function movies() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the movie title, please.",
+            name: "request"
+        }
+    ]).then(function (media) {
+        var movieTitle = media.request.split(" ").join("+");
+        if (!movieTitle) {
+            movieTitle = "Mr. Nobody";
+        }
+        axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=" + keys.omdb.moviesAPI).then(
+            function (response) {
+                    try {
+                        var moviesData = response.data;
+                        var movieTitle = moviesData.Title;
+                        var movieYear = moviesData.Year;
+                        var imdbRating = moviesData.imdbRating;
+                        var rottenRating = moviesData.Ratings[1].Value;
+                        var country = moviesData.Country;
+                        var language = moviesData.Language;
+                        var plot = moviesData.Plot;
+                        var actors = moviesData.Actors;
+                        
+                        console.log(`${divider}`);
+                        console.log(`Title & Year: ${movieTitle}, made in ${movieYear}`);
+                        console.log(`IMdB's rating is: ${imdbRating}`);
+                        console.log(`Rotton Tomatoes says: ${rottenRating}`);
+                        console.log(`Country where produced: ${country}`);
+                        console.log(`Language: ${language}`);
+                        console.log(`Plot: ${plot}`);
+                        console.log(`Actors: ${actors}`);
+                        console.log(`${divider}`);
+                        makeChoice();
 
-//                                 //console.log(response);
-//                                 if (error) {
-//                                         console.log(error);
-//                                         return;
-//                                 } else {
-//                                         var moviesData = response.data;
-//                                         console.log(`
-//                                         \nTitle & Year: ${moviesData.Title}, made in ${moviesData.Year}
-//                                         \nIMdB's rating is: ${moviesData.imdbRating}
-//                                         \nRotton Tomatoes says: ${moviesData.Ratings[1].Value}
-//                                         \nCountry where produced: ${moviesData.Country}
-//                                         \nLanguage: ${moviesData.Language}
-//                                         \nPlot: ${moviesData.Plot}
-//                                         \nActors: ${moviesData.Actors}
-//                                         \n${divider}
-//                                 `)
-//                                 }
-//                         }
-//                 );
-//         }
-// };
-// function other() {
-//         if (command === "do-what-it-says") {
-//                 fs.readFile("random.txt", "utf8", function (error, data) {
-//                         if (error) {
-//                                 return console.log(error);
-//                         }
-//                         console.log(data);
-//                 })
-//         }
-// };
-concerts();
-// songs();
-// movies();
-// other();
+                    } catch (error) {
+                        console.log("Error: " + error);
+                    }
+            }, function (error) {
+                console.log("Error: ");
+                if (error) {
+                    console.log(error);
+                }
+            }
+
+        );
+    });
+}
+function other() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var textFile = data.split(",");
+        userRequest = textFile[1];
+        switch (textFile[0]) {
+            case "concert-this":
+                concerts();
+                break;
+            case "spotify-this-song":
+                songs();
+                break;
+            case "movie-this":
+                movies();
+                break;
+            default:
+                makeChoice();
+        }
+        console.log(data);
+    })
+};
 
 
-
-
-
-//  spotify-this-song
-    //Use node-spotify-api
-        //node liri.js spotify-this-song '<song name here>'
-        //  Render: Artist(s)
-        //  The song's name
-        //  A preview link of the song from Spotify
-        //  The album that the song is from
-            //  If no song, default to "The Sign" by Ace of Base
-
-//  do-what-it-says
-    //fs Node package, take random.txt and use it call a command
-        //node liri.js do-what-it-says
-        //Should run spotify-this-song for "I Want it That Way" from random.txt
-        //Test text in random.txt for movie-this and concert-this
 //Bonus
     //Log output to log.txt
     //Append each command to log.txt, do not overwrite the file each time
